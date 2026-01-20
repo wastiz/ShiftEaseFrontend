130a182f332516a6fd1df2fa05677e5d9b1a42fa
Admin <admin@Admins-MacBook-Pro.local>
2026-01-20 18:49:01 +0200

    test commit

--- src/app/(private)/(employer)/schedules/page.tsx
+++ src/app/(private)/(employer)/schedules/page.tsx
@@ -14,6 +14,7 @@ import { CheckCircle2, X } from "lucide-react";
 import { useState, useEffect } from "react";
 
 export default function Schedules() {
+    //test comment
     const router = useRouter()
     const [showSuccessCard, setShowSuccessCard] = useState(false)
 
